4e06c069eaca234f7912434dc3b1bf47eff89de5
corton30 <abdelrahman.m.hamdy@gmail.com>
2025-04-19 00:00:44 +0200

    added nss

--- backend/models/Patient.js
+++ backend/models/Patient.js
@@ -6,7 +6,12 @@ const patientSchema = new mongoose.Schema({
   date_naissance: { type: Date, required: true },
   antecedents: { type: String },
   allergies_connues: { type: Boolean, default: false },
-  date_creation: { type: Date, default: Date.now }
+  date_creation: { type: Date, default: Date.now },
+  nss: { type: Number, required: true, unique: true },
+  telephone: { type: String, required: true },
+  adresse: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  sexe: { type: String, enum: ["Homme", "Femme"], required: true }
 });
 
 module.exports = mongoose.model("Patient", patientSchema);
